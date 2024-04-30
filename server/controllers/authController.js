2ceaa11b36da7cbee64110211b5185b0162e758a
Dubem Ezeagwu <dubemezeagwu@gmail.com>
2024-04-30 06:36:15 +0100

    edit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 const AppError = require("./../utils/appError");
 
 exports.signup = catchAsync(async (req, res, next) => {
