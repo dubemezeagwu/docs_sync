98b01d4e46d5f3563f126e415544aad5473cba05
Dubem Ezeagwu <dubemezeagwu@gmail.com>
2024-05-08 07:27:18 +0100

    chore: cleanup & added app constants

--- app/lib/core/constants/color_constants.dart
+++ app/lib/core/constants/color_constants.dart
@@ -1,4 +1,4 @@
-import "package:docs_sync/core/app_core.dart";
+import 'package:docs_sync/screens/app_screens.dart';
 
 const Color kWhite = Color(0xFFFFFFFF);
 const Color kBlack = Color(0xFF000000);
---
+++ app/lib/core/constants/image_constants.dart
@@ -0,0 +1,4 @@
+class AppAssets {
+  static String google = "app/assets/svg/google.svg";
+  static String note = "app/assets/svg/note.svg";
+}
