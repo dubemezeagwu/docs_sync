d9065c7067f3e1c5d0125dea2c11955eef9dae59
Dubem Ezeagwu <dubemezeagwu@gmail.com>
2024-05-18 00:37:16 +0100

    fix: bug on greetings

--- app/lib/core/utils/extensions.dart
+++ app/lib/core/utils/extensions.dart
@@ -49,9 +49,7 @@ extension DateTimeExtension on DateTime {
   }
 
   String get timeOfDay {
-    if (hour >= 0 && hour < 6) {
-      return "Night";
-    } else if (hour >= 6 && hour < 12) {
+    if (hour >= 0 && hour < 12) {
       return "Morning";
     } else if (hour >= 12 && hour < 18) {
       return "Afternoon";
