e178155bcc73a6ea82fbb6b741dd4a25eaaeeea8
Dubem Ezeagwu <48288857+dubemezeagwu@users.noreply.github.com>
2024-05-23 13:46:40 +0100

    collab editing: UI and controllers from server
    * wip: Ui for add collaborators bottom sheet
    
    * add: controllers & routes for collab

--- app/lib/screens/app_screens.dart
+++ app/lib/screens/app_screens.dart
@@ -11,6 +11,7 @@ export "widgets/main_appbar.dart";
 export "widgets/document_list_widget.dart";
 export "widgets/loading_screen.dart";
 export "widgets/shadow_card.dart";
+export "widgets/collaborators_bottom_sheet.dart";
 
 export 'package:docs_sync/core/app_core.dart';
 
